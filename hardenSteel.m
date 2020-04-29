10dcb1e3875a9d8d40b06286caefb9a931b83368
jyyblue <jyyblue1987@outlook.com>
2020-04-29 18:21:46 +0800

    [add] add  annotation.

--- hardenSteel.m
+++ hardenSteel.m
@@ -5,10 +5,18 @@ for hour = [1, 8, 24, 48]
     y = ones(1, n);
     for i = 1:n
         y(i) = carbon(x(i), t);        
-    end
+    end    
     plot(x, y);
     hold on
 end    
+xlabel('distance x (in meters) from the surface');
+ylabel('the concentration of carbon C(x; t)');
+title('the carbon concentration at 1, 8, 24, 48 hour');
+
+legend('hour=1','hour=8','hour=24','hour=48')
+
 hold off
+
+
 
 
