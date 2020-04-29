8148d1ce22e5a92139513d756960784364e21a99
jyyblue <jyyblue1987@outlook.com>
2020-04-29 17:29:15 +0800

    [add] interpolation application.

---
+++ interpolateClimate.m
@@ -0,0 +1,21 @@
+xi = 1885:10:1995;
+fi = [56.70 56.72 56.87 56.89 57.01 57.21 57.28 57.18 57.12 57.22 57.65 57.89];
+
+x = linspace(1885, 1995, 200);
+y = Lagrange(xi, fi, x);
+
+plot(x, y);
+hold on;
+
+plot(xi, fi, 'o');
+hold off;
+
+x_data = [1903 1941 1963 1969 1976 1989 1999 2009];
+y_pred = Lagrange(xi, fi, x_data)
+y_real = [56.71 57.40 57.25 57.20 56.79 57.69 57.97 58.50];
+
+err = norm(y_pred - y_real);
+disp(['Interpolation error = ', num2str(err)]);
+
+y_2026 = Lagrange(xi, fi, [2026]);
+disp(['2026 year predict temperature = ', num2str(y_2026)]);
